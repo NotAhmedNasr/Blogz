fc8ede5cf7f845bc963e3e4cf7a05d2dcd97e938
Ahmed Nasr <ahmed0alinasr@gmail.com>
2021-01-12 21:32:42 +0200

    2
    timestamps, indexes, blog get

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const {getAll} = require('../controllers/blog');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+router.get('/', async (req, res, next) => {
+  try {
+    const blogs = await getAll();
+    res.json(blogs);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
