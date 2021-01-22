6363a018560b56d98e0bb3cf386aa27fb5ada7e2
Ahmed Nasr <ahmed0alinasr@gmail.com>
2021-01-22 16:59:10 +0200

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 const errorHandler = require('./middlewares/Error');
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost:27017/blogs', {useNewUrlParser: true, useU
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/', routes);
 
