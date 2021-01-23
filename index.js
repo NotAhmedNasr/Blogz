2e57231e40b823a261b768c76b3c75b14ce76d4f
Ahmed Nasr <ahmed0alinasr@gmail.com>
2021-01-23 16:35:28 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+const errorHandler = require('./middlewares/Error');
+
+const dburi = process.env.MONGODB_URI;
+
+mongoose.connect(dburi,
+    {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+    .then(() => console.log('Database Connected Successfully'))
+    .catch((err) => console.log(err));
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/static/images', express.static('private/uploads/images'));
+
+app.use('/', routes);
+
+app.use((req, res, next) => {
+  res.status(404).end('NOT FOUND');
+});
+
+app.use(errorHandler);
+
+const {PORT = 8000} = process.env;
+
+app.listen(PORT, () => {
+  console.log(`Application is listening on http://localhost:${PORT}`);
+});
